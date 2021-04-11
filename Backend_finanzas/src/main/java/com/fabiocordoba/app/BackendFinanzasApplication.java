c9115f60a1029d9b67ddda7909cae7a095e52c6f
Fabio Cordoba <fabiocordoba1@gmail.com>
2021-04-11 10:40:12 -0500

    base SpringBoot

---
+++ Backend_finanzas/src/main/java/com/fabiocordoba/app/BackendFinanzasApplication.java
@@ -0,0 +1,13 @@
+package com.fabiocordoba.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFinanzasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFinanzasApplication.class, args);
+	}
+
+}
---
+++ Backend_finanzas/src/test/java/com/fabiocordoba/app/BackendFinanzasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiocordoba.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFinanzasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
